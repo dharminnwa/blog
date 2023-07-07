8fc5a61ce8379ba20d3e94c558392cbeca9dd227
Tushar Morker <tusharm.wa@gmail.com>
2023-07-07 15:06:20 +0530

    design of title of blog, home and users

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home-routing.module';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -9,7 +10,8 @@ import { HomeRoutingModule } from './home-routing.module';
   ],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    MatIconModule,
   ]
 })
 export class HomeModule { }
