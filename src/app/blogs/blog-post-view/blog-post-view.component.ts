7d487fb0835737d6983476a5e9734e845e49dfe6
Jainishbwa <Jainishb.wa@gmail.com>
2023-07-08 13:29:46 +0530

    Fixed home card and blog height issue

--- src/app/blogs/blog-post-view/blog-post-view.component.ts
+++ src/app/blogs/blog-post-view/blog-post-view.component.ts
@@ -8,8 +8,8 @@ import { SharedService } from 'src/app/shared/service/shared.service';
   styleUrls: ['./blog-post-view.component.sass']
 })
 export class BlogPostViewComponent implements OnInit {
-  @Input('blogPost')blogPost:IBlogPostApiModel[] = []
-  isExpanded:boolean = false;
+  @Input('blogPost') blogPost: IBlogPostApiModel[] = []
+  isExpanded: boolean = false;
 
   constructor(
     private sharedService: SharedService
@@ -18,25 +18,32 @@ export class BlogPostViewComponent implements OnInit {
   ngOnInit(): void {
     this.getBlogPosts();
   }
-  
+
   getBlogPosts() {
     this.sharedService.getBlogPosts().subscribe({
-     next:(res) => {
-       this.blogPost = res;
-       this.blogPost.forEach((post) => { 
-        post.isExpanded = false;
-        post.maxHeight = '35px';
-      })
-     }
+      next: (res) => {
+        this.blogPost = res;
+        this.blogPost[0].body = 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut';
+        this.blogPost[1].body = 'quia et suscipit\nsuscipit recusandae consequuntur';
+    this.blogPost[5].body = 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut';
+    this.blogPost[9].body = 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut';
+        this.blogPost.forEach((post) => {
+          post.isExpanded = false;
+          post.maxHeight = '110px';
+        })
+      }
     })
- }
+    
+  }
 
- toggleAccordion(): void {
-  this.isExpanded = !this.isExpanded;
-}
+  toggleAccordion(): void {
+    this.isExpanded = !this.isExpanded;
+  }
 
-togglePost(post: any): void {
-  post.isExpanded = !post.isExpanded;
-}
+  togglePost(post: any): void {
+    post.isExpanded = !post.isExpanded;
+    this.isExpanded = !this.isExpanded;
+    console.log(post.isExpanded)
+  }
 
 }
